e1770ecdc9e38d85904a7c6a981c885a7dfb1e99
Mikaila Brown <mbr2201@wgu.edu>
2024-05-06 08:59:01 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Canvas from "./canvas"
+import Cusomizer from "./pages/Cusomizer"
+import Home from "./pages/Home"
+
+
+function App() {
+  
+
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Cusomizer />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton";
+import Tab from "./Tab";
+import FilePicker from "./FilePicker";
+import ColorPicker from "./ColorPicker";
+import AIPicker from "./AIPicker";
+
+export {
+    CustomButton,
+    AIPicker,
+    Tab,
+    FilePicker,
+    ColorPicker,
+}
